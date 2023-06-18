8fd9de52023f6b123b03859a84fa10dd547a4767
MeowKawaii <nanthikan.aw@gmail.com>
2023-06-18 23:32:47 +0700

    add default style

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      primary: 'var(--color-primary)',
+      secondary: 'var(--color-secondary)',
+      white: 'var(--color-white)',
+      black: 'var(--color-black)',
+    },
     extend: {
+      maxWidth: {
+        '1400': '1400px',
+      }
     },
   },
   plugins: [],
